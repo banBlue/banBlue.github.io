d727e7506a797fd0d0303dae0b5d91b2dc96fc43
huangmingyi <huangmingyi@smyfinancial.com>
2023-08-03 11:40:46 +0800

    .

--- mm.js
+++ mm.js
@@ -1,7 +1,7 @@
-const data = [
+[
   {
-    name:'国实',
-    dd:'JC72gRIVyZ4L/01HDWkFesUoLKeJXhmv6n5XT5XuIIY7rPqoxluSgOEZGd+fz7OFrjU3dhFsi2KbuLyiRg9ZCQ+yXVMLrFm+DbOeA1w9slhYkfA3eyp0kNf3uBr2zwn23b91boNnUHahDIu4acCDCMBZ2WAANZntmdXM2BMxOkk=',
-    io:'SQfOJ4pqTqVGo+SGHsr2VGNYJVqNg2ezVSKiwpUhahXLtruv1/Zgk9yahjytsm7EllmCIwm8lDOQAXPqfyq8nDnfk+Dn0XDZusEDLN5tPh6VfQXzyc7NvmkNteYNKSJfQuq2thCpOupbITYt5PX0Ok+m3hn5/jgWF4y6Ox81AgM='
+    "name":"国实",
+    "dd":"JC72gRIVyZ4L/01HDWkFesUoLKeJXhmv6n5XT5XuIIY7rPqoxluSgOEZGd+fz7OFrjU3dhFsi2KbuLyiRg9ZCQ+yXVMLrFm+DbOeA1w9slhYkfA3eyp0kNf3uBr2zwn23b91boNnUHahDIu4acCDCMBZ2WAANZntmdXM2BMxOkk=",
+    "io":"SQfOJ4pqTqVGo+SGHsr2VGNYJVqNg2ezVSKiwpUhahXLtruv1/Zgk9yahjytsm7EllmCIwm8lDOQAXPqfyq8nDnfk+Dn0XDZusEDLN5tPh6VfQXzyc7NvmkNteYNKSJfQuq2thCpOupbITYt5PX0Ok+m3hn5/jgWF4y6Ox81AgM="
   }
 ]
